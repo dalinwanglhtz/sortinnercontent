772daecd211e730d63d5555bf4a22b616f270a64
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-05 21:46:24 +1100

    first working cut

--- src/main/java/com/codewardev/Kata.java
+++ src/main/java/com/codewardev/Kata.java
@@ -1,5 +1,7 @@
 package com.codewardev;
 
+// https://www.codewars.com/kata/5898b4b71d298e51b600014b/train/java
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
@@ -7,12 +9,20 @@ import java.util.stream.Collectors;
 public class Kata {
 
 	public static String sortTheInnerContent(String words) {
+		System.out.println("Words: "+words);
+		StringBuilder result = new StringBuilder();
 		for(String x:words.split(" ")) {
+			if(x.length() < 3) {
+				result.append(x+" ");
+				continue;
+			}
 			String word = x.substring(1, x.length()-1);
 			word = Arrays.stream(word.split("")).sorted(Collections.reverseOrder()).collect(Collectors.joining());
-			System.out.println("X: "+x+" to "+word);
+			word = x.toCharArray()[0]+word+x.toCharArray()[x.length()-1];
+			//System.out.println("X: "+x+" to "+word);
+			result.append(word+" ");
 		}
-		return null;
+		return result.toString().trim();
 	}
 
 }
