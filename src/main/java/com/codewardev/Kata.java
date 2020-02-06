3f67b406c491a059c25c2c9ac1c65077fd60dbe1
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-06 22:48:18 +1100

    refined working cut

--- src/main/java/com/codewardev/Kata.java
+++ src/main/java/com/codewardev/Kata.java
@@ -1,26 +1,26 @@
 package com.codewardev;
 
+
 // https://www.codewars.com/kata/5898b4b71d298e51b600014b/train/java
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 public class Kata {
 
 	public static String sortTheInnerContent(String words) {
-		System.out.println("Words: "+words);
 		StringBuilder result = new StringBuilder();
 		for(String x:words.split(" ")) {
 			if(x.length() < 3) {
 				result.append(x+" ");
-				continue;
+			} else {
+			result.append(x.charAt(0)+new StringBuilder()
+					.append(x.substring(1, x.length()-1)
+							.codePoints()
+							.map(Character::toLowerCase)
+							.sorted()
+							.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append))
+					.reverse()
+					.toString()+x.charAt(x.length()-1)+" ");
 			}
-			String word = x.substring(1, x.length()-1);
-			word = Arrays.stream(word.split("")).sorted(Collections.reverseOrder()).collect(Collectors.joining());
-			word = x.toCharArray()[0]+word+x.toCharArray()[x.length()-1];
-			//System.out.println("X: "+x+" to "+word);
-			result.append(word+" ");
 		}
 		return result.toString().trim();
 	}
